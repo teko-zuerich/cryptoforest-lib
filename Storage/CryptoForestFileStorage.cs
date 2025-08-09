6b492cea0312ff34c41badf59315bb8f1c155011
Silvan Bauer <silvan.bauer@hotmail.ch>
2025-08-09 11:02:13 +0200

    Adding the storage system for the CryptoForest
    Added the interface ICryptoForestStorage and the implementation
    CryptoForestFileStorage. The CryptoForestFileStorage
    implementation can be used to handle a CryptoForest on the
    file system.

---
+++ Storage/CryptoForestFileStorage.cs
@@ -0,0 +1,49 @@
+﻿namespace CryptoForestLibrary.Storage;
+public class CryptoForestFileStorage : ICryptoForestStorage
+{
+    private readonly string _cryptoForestDirectory;
+
+    /// <summary>
+    /// Creates a storage handler for the file system
+    /// </summary>
+    /// <param name="cryptoForestDirectory">The directory where the CryptoForest lies</param>
+    /// <exception cref="ArgumentException">Thrown when the directory passed is not valid, not accessible or doesn't exist</exception>
+    public CryptoForestFileStorage(string cryptoForestDirectory)
+    {
+        if (!Directory.Exists(cryptoForestDirectory))
+        {
+            throw new ArgumentException("The directory path is invalid, not accessible or doesn't exist", nameof(cryptoForestDirectory));
+        }
+
+        // Sets the _cryptoForestDirectory and removes / or \ at the end of the path
+        _cryptoForestDirectory = cryptoForestDirectory.EndsWith('/') || cryptoForestDirectory.EndsWith('\\') ? cryptoForestDirectory[..^1] : cryptoForestDirectory;
+    }
+
+    /// <summary>
+    /// Creates a FileStream for reading or writing an entry in the CryptoForest
+    /// </summary>
+    /// <param name="entryGuid">The GUID of the entry to create or read</param>
+    /// <param name="asReadonly">Specifies whether or not the stream is reading or creating a file</param>
+    /// <returns>Returns the FileStream for the specified GUID</returns>
+    /// <exception cref="ArgumentException">Throws when the entry GUID that should be read doesn't exist</exception>
+    public Stream GetStream(Guid entryGuid, bool asReadonly)
+    {
+        var entryPath = $"{_cryptoForestDirectory}/{entryGuid}";
+        if (asReadonly)
+        {
+            if (!File.Exists(entryPath))
+            {
+                throw new ArgumentException($"Cannot read the entry {entryGuid} as it doesn't exist", nameof(entryGuid));
+            }
+
+            return new FileStream(entryPath, FileMode.Open, FileAccess.Read);
+        }
+
+        return new FileStream(entryPath, FileMode.Create, FileAccess.Write);
+    }
+
+    public Task FinalizeAsync(Stream storageStream, CancellationToken cancellationToken = default)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ Storage/ICryptoForestStorage.cs
@@ -0,0 +1,22 @@
+﻿namespace CryptoForestLibrary.Storage;
+
+/// <summary>
+/// The interface used to define a storage system implementation for the CryptoForest
+/// </summary>
+public interface ICryptoForestStorage
+{
+    /// <summary>
+    /// Creates the storage stream used in the CryptoForestCryptograph
+    /// </summary>
+    /// <param name="entryGuid">The GUID of the entry this stream belongs to</param>
+    /// <param name="asReadonly">If the Stream should be created as readonly or if it should be writable</param>
+    /// <returns>Returns the stream for the entry</returns>
+    public Stream GetStream(Guid entryGuid, bool asReadonly);
+
+    /// <summary>
+    /// Called after the operations are finished in the CryptoForestCryptograph.
+    /// Can be used to define actions for the storage after the operations have finished.
+    /// </summary>
+    /// <param name="storageStream">The current storage stream</param>
+    public Task FinalizeAsync(Stream storageStream, CancellationToken cancellationToken = default);
+}
